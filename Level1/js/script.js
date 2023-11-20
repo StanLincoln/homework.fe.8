3fe5420510b39048dc15b3bf8a4a7f34ae2cfffa
Stan Lincoln <stanizbash@gmai.com>
2023-11-20 19:27:45 +0100

    lS

--- Level1/js/script.js
+++ Level1/js/script.js
@@ -1,9 +1,13 @@
-const switcher = document.querySelector(".slider");
+
+const switcher = document.querySelector('input[type="checkbox"]')
 const currentTheme = localStorage.getItem("theme");
-if (currentTheme) {
-  document.body.classList.add(currentTheme);
+if (document.body.classList.contains('dark-mode')) {
+  console.log(switcher)
+  console.log(switcher.checked)
+  switcher.checked = true
 }
 switcher.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   localStorage.setItem("theme", document.body.classList);
 });
+
